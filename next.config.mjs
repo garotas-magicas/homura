6309c790b28d0e634e1c43c934caa1c194afd235
vergonha <shirosayuya@gmail.com>
2024-07-10 15:52:57 -0300

    vai tomar no cu nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compress: false,
 };
 
-export default nextConfig;
+export default {
+  ...nextConfig,
+    webpack(webpackConfig) {
+      return {
+        ...webpackConfig,
+        optimization: {
+          minimize: false
+        }
+      };
+    }
+};
