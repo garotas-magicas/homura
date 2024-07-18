98e25ddc4daf8d91fee742c4363a16da3c706f8c
nica <100009274+nicaksks@users.noreply.github.com>
2024-07-18 20:15:09 -0300

    BARRA REMOVIDA, O INIMIGO AGORA É OUTRO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <main className="bg-madoka-black h-screen font-ubuntu">
-      \
       <div className="w-100 flex items-center justify-center flex-col gap-5">
         <div className="w-100 pt-32">
           <Image src={"/renders/header.gif"} alt="" width={500} height={250} />
